d6c0d297ebacd874cdd604f365a317706dc9f17a
dayonxxi <dayoni99@gmail.com>
2022-09-06 21:50:33 +0900

    Add file upload (but with some error)

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
@@ -17,3 +18,7 @@ export const firebaseInstance = firebase;
 export const authService = firebase.auth();
 // Initialize Cloud Firestore and get a reference to the service
 export const dbService = firebase.firestore();
+// 사진, 동영상 같은 파일을 저장하기 위해 파이어베이스 스토리지
+// 스토리지는 파이어스토어와 달리 문서에 아이디를 자동으로 만들어주지 않기 때문에
+// 고유 십결자를 만들어서 스토리지에 저장할 데이터에 짝을 지어줘야 함 => uuid 라이브러리 사용하기
+export const storageService = firebase.storage();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,32 +1,45 @@
-import { dbService } from 'fbase';
+import { dbService, storageService } from 'fbase';
 import React, { useEffect, useState } from 'react';
 import Nweet from 'components/Nweet';
+import { v4 as uuid4 } from 'uuid';
 
 const Home = ({ userObj }) => {
 	// console.log(userObj);
 	const [nweet, setNweet] = useState('');
 	const [nweets, setNweets] = useState([]);
+	const [attachment, setAttachment] = useState('');
 
 	useEffect(() => {
-		dbService.collection('nweets').onSnapshot((onSnapshot) => {
-			const newArray = onSnapshot.docs.map((document) => ({
-				id: document.id,
-				...document.data(),
-			}));
-			setNweets(newArray);
-		});
+		dbService
+			.collection('nweets')
+			.orderBy('createdAt', 'desc')
+			.onSnapshot((onSnapshot) => {
+				const newArray = onSnapshot.docs.map((document) => ({
+					id: document.id,
+					...document.data(),
+				}));
+				setNweets(newArray);
+			});
 	}, []);
 
 	const onSubmit = async (event) => {
 		event.preventDefault();
 		// 컬렉션 생성 후, add()로 해당 컬렉션에 문서 생성 및 값 저장
-		await dbService.collection('nweets').add({
+		/* await dbService.collection('nweets').add({
 			text: nweet,
 			createdAt: Date.now(),
 			createdID: userObj.uid,
 		});
 		// nweet 상태를 다시 빈 문자열로 초기화
-		setNweet('');
+		setNweet(''); */
+
+		// 스토리지, 레퍼런스를 순서대로 호출한 다음
+		// child함수에 사용자 아이디를 폴더 이름으로, 파일 이름을 uuid4로 처리함
+		const attachmentRef = storageService
+			.ref()
+			.child(`${userObj.uid}/${uuid4()}`);
+		const response = await attachmentRef.putString(attachment, 'data_url');
+		console.log(response);
 	};
 
 	const onChange = (event) => {
@@ -38,7 +51,26 @@ const Home = ({ userObj }) => {
 		setNweet(value);
 	};
 
-	// console.log(nweets);
+	const onFileChange = (event) => {
+		// console.log(event.target.files);
+		const {
+			target: { files },
+		} = event;
+		const theFile = files[0];
+		const reader = new FileReader();
+		// onloadend는 파일이 함수로 들어간 이후 결괏값이 나온 다음 상황을 감지하는데,
+		// 그때 생긴 이벤트값을 사용할 수 있게 해줌 (이벤트값에는 우리가 원하는 파일 URL이 있음)
+		reader.onloadend = (finishedEvent) => {
+			const {
+				currentTarget: { result },
+			} = finishedEvent;
+			setAttachment(result);
+		};
+		// readAsDataURL 함수는 파일 정보를 인자로 받아서 파일 위치를 URL로 반환함
+		reader.readAsDataURL(theFile);
+	};
+
+	const onClearAttachment = () => setAttachment('');
 
 	return (
 		<div>
@@ -50,7 +82,14 @@ const Home = ({ userObj }) => {
 					placeholder="what's on your mind?"
 					maxLength={120}
 				/>
+				<input type='file' accept='image/*' onChange={onFileChange} />
 				<input type='submit' value='Nweet' />
+				{attachment && (
+					<div>
+						<img src={attachment} alt='nweet_img' width='50px' height='50px' />
+						<button onClick={onClearAttachment}>Clear</button>
+					</div>
+				)}
 			</form>
 			{nweets.map((nweet) => (
 				<Nweet
