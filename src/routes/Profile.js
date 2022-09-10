4c4487ed50b4493d95bca4e84f0e63f098d00ffe
dayonxxi <dayoni99@gmail.com>
2022-09-10 18:15:44 +0900

    Add nweets filtering & Fix errors

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -17,14 +17,14 @@ const Profile = ({ userObj }) => {
 			.orderBy('createdAt', 'asc')
 			.get(); // get함수는 쿼리 문을 통해 얻은 결과문을 가져옴
 
-		console.log(nweets);
 		console.log(nweets.docs.map((doc) => doc.data()));
 	};
 
 	// Profile 컴포넌트가 렌더링된 이후 useEffect의 첫번째 인자로 넘겨준 함수가 실행됨
 	useEffect(() => {
 		getMyNweets();
-	},[];
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [userObj]);
 
 	return (
 		<>
